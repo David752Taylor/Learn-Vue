f9c28353c433b2031c0b940203b69d835b4e59b5
zsl <2499396475@qq.com>
2025-05-08 20:01:55 +0800

    vuex通信基础1

---
+++ 01-辅助函数mapState/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  strict: true,
+  state: {
+    title: '大标题',
+    count: 100
+  },
+  mutations: {
+    addCount (state, n) {
+      state.count += n
+    },
+    subCount (state, n) {
+      state.count -= n
+    },
+    changeTitle (state, newTitle) {
+      state.title = newTitle
+    },
+    changeCount (state, newCount) {
+      state.count = newCount
+    }
+  }
+})
+
+export default store
